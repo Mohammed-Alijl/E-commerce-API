cb11dec168311680abc42e3045c1a4f746063a29
Mohammed-Alijl <moh.wa.2016@gmail.com>
2022-09-26 17:59:57 +0300

    //

--- app/Http/Requests/Api/product/IndexRequest.php
+++ app/Http/Requests/Api/product/IndexRequest.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Requests\Api\product;
 
-use App\Http\Controllers\Api\Traits\Api_Response;
 use App\Http\Resources\Product\IndexResource;
-use App\Models\Image;
 use App\Models\Product;
 use Exception;
 use Illuminate\Foundation\Http\FormRequest;
 
 class IndexRequest extends FormRequest
 {
-    use Api_Response;
+
 
     public function authorize()
     {
@@ -21,13 +19,9 @@ class IndexRequest extends FormRequest
     public function run()
     {
         try {
-//            return $this->apiResponse(Product::select('id','name','price')->with(['images'=>function($q){
-//                $q->select('product_id','image');
-//            }])->paginate(2), 200, 'This is all products in the category');
-            $products = IndexResource::collection(Product::paginate(2));
-            return $this->apiResponse($products,200,'This is all products in the category');
+            return IndexResource::collection(Product::paginate(2));
         } catch (Exception $ex) {
-            return $this->apiResponse(null, 400, $ex->getMessage());
+            return $this->apiResponse(null, 500, $ex->getMessage());
         }
     }
 
